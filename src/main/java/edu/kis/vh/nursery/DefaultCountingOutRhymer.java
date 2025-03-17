1de5ecc7204eff9253b4a76e144dec57da8358a8
251160 <251160@helium.com>
2025-03-17 11:38:57 +0100

    6. Replacing literal values with constants

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,7 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int ARR_SIZE = 12;
+    public static final int FALSE_AS_INT = -1;
+    private int[] numbers = new int[ARR_SIZE];
 
     public int total = -1;
 
@@ -12,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == FALSE_AS_INT;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == ARR_SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return FALSE_AS_INT;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return FALSE_AS_INT;
         return numbers[total--];
     }
 

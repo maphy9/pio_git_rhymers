8087b44417985b4c7f1edbe9664ad4adf07f29ba
maphy9 <maphy9@gmail.com>
2025-03-24 11:32:29 +0100

    10. Generate getter and setter for total field

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,6 +9,14 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_ARRAY;
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;

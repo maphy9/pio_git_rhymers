8d81bfd5366282411bcc04646d8d19db76a9e213
maphy9 <maphy9@gmail.com>
2025-03-25 17:02:34 +0100

    17. Add documentation for IntLinkedList class

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,18 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Linked list data structure implementation for int data type
+ */
 public class IntLinkedList {
 
     private static final int IS_EMPTY = -1;
     private Node last;
     private int i;
 
+    /**
+     * Adds i to the back of the linked list
+     * @param i
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -16,20 +23,36 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Returns true if the linked list is empty and false otherwise
+     * @return
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Returns true if the linked list is full and false otherwise
+     * @return
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Returns the value of the last node in linked list
+     * @return
+     */
     public int top() {
         if (isEmpty())
             return IS_EMPTY;
         return last.getValue();
     }
 
+    /**
+     * Removes the last node from the linked list and returns its value
+     * @return
+     */
     public int pop() {
         if (isEmpty())
             return IS_EMPTY;
